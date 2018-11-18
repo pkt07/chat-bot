fe978660c66df7974a0f19816df84f73e40d937b
pkt07 <tripathi.prabhat1996@gmail.com>
2018-11-18 18:07:53 +0530

    post and get message

--- server.js
+++ server.js
@@ -1,14 +1,48 @@
 var express = require("express")
+var mongoose = require("mongoose")
 var app = express();
 var bodyparser = require("body-parser");
-app.use(bodyparser.urlencoded({extended:false}));
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
 
 
-app.use(express.static(__dirname));
-app.get('/', function(req, res) {
-        res.sendFile(__dirname + "/" + "index.html"); // in case of frontend req
-        });
+var conString = "mongodb://prabhat:prabhat123@ds211694.mlab.com:11694/chat-bot";
+app.use(express.static(__dirname))
 
+var Chats = new mongoose.Schema({
+  name: String,
+    chat: String
+});
+
+var User = mongoose.model("User", Chats);
+mongoose.connect(conString, { useMongoClient: true }, (err) => {
+    console.log("Database connection", err)
+})
+
+app.post("/chats", (req, res) => {
+    var chat = new User(req.body);
+    console.log("yahan toh aaya");
+  	chat.save()
+    .then(item => {
+      res.send("item saved to database");
+    })
+    .catch(err => {
+      res.status(400).send("unable to save to database");
+    });
+});
+
+app.get("/chats", (req, res) => {
+    User.find({}, (error, chats) => {
+        res.send(chats);
+    })
+});
 var server = app.listen(3000,function(){
 	console.log("connected");
 });
+
+
+
+
+
+
+
